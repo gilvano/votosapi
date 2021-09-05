c9b3740e716a15dd673edb797135fce80cd3beb5
Gilvano Piontkoski <g.piontkoski@gmail.com>
2021-09-05 15:00:31 -0300

    Criação do projeto

---
+++ src/main/java/com/gilvano/votosapi/VotosApiApplication.java
@@ -0,0 +1,13 @@
+package com.gilvano.votosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gilvano/votosapi/VotosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilvano.votosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
