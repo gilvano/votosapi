3c8a786b6091f1daf7452a9fe3dad25a570b9ffe
Gilvano Piontkoski <g.piontkoski@gmail.com>
2021-09-05 23:06:17 -0300

    Ajustar descricao do controller

--- src/main/java/com/gilvano/votosapi/controller/AssociadoController.java
+++ src/main/java/com/gilvano/votosapi/controller/AssociadoController.java
@@ -3,7 +3,6 @@ package com.gilvano.votosapi.controller;
 import java.util.List;
 
 import com.gilvano.votosapi.model.Associado;
-import com.gilvano.votosapi.repository.AssociadoRepository;
 import com.gilvano.votosapi.service.AssociadoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +16,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/associado")
 @AllArgsConstructor
+@Tag(name = "Pauta", description = "Controller Pauta")
 public class AssociadoController {
 
     @Autowired
@@ -29,12 +31,14 @@ public class AssociadoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Cadastrar um novo associado")
     public Associado salvar(@RequestBody Associado associado){
         return associadoService.salvar(associado);
     }
 
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Buscar um associado por Id")
     public Associado buscarPorId(@PathVariable Long id){
         return associadoService.BuscarPorId(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Associado nao encontrado." ));
@@ -42,6 +46,7 @@ public class AssociadoController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Buscar todos os associados cadastrados")
     public List<Associado> buscarTodos(){
         return associadoService.buscarTodos();
     }   
